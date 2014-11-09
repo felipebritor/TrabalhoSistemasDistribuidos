5ec4bd3e2adda6056ecdaa9fcbb9022dedbc1c88
Felipe Brito <felipebritor@gmail.com>
2014-11-09 00:44:05 -0200

    Criação Executor
    Criação da classe Executor

--- src/thread/Executor.java
+++ src/thread/Executor.java
@@ -5,10 +5,34 @@
  */
 package thread;
 
-/**
- *
- * @author Avell B153
- */
-public class Executor {
+import servidor.Ponto;
+
+public class Executor implements Runnable{
+
+    private Monitor mm;
+    private int id;
+    private int pontosNoCirculo;
+    private Ponto[] pontos;
+    
+    public Executor(int id, Ponto[] pontos, Monitor monitor){
+        this.id = id;
+        this.pontos = pontos;
+        this.mm = monitor;
+        pontosNoCirculo = 0;
+    }
+    
+    @Override
+    public void run() {
+         synchronized (mm) {
+             for (Ponto ponto : pontos) {
+                 float sqrt = (ponto.x * ponto.x) + (ponto.y * ponto.y);
+                 if(sqrt <= 1)
+                     pontosNoCirculo++;
+                 if(mm.getStatus() == 1) //CHECAR
+                     break;
+             }
+            System.out.println("Thread id"+id+": "+pontosNoCirculo);
+         }
+    }
     
 }
