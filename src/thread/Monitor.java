6d9d01735b60915465f48643f128c7debecdb779
Felipe Brito <felipebritor@gmail.com>
2014-11-08 23:54:50 -0200

    Criação Monitor
    Criação da Classe Monitor

---
+++ src/thread/Monitor.java
@@ -0,0 +1,12 @@
+package thread;
+
+public class Monitor {
+    private int status = 0;
+    
+    public int getStatus() {
+        return status;
+    }
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
